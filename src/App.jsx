e7529904621d173b46aebb36b5d58972a614b1ce
Athul-Sagar <athulsagar@gmail.com>
2025-03-16 03:53:18 +0530

    TicTacToe Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import "./App.css";
+import UseTicTacToe from "./hooks/UseTicTacToe";
 
 const App = () => {
+  const { board, handleClick, calculateWinner, resetGame, getStatusMessage } = UseTicTacToe();
+  
+  const winner = calculateWinner(board);
+  
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="game">
+      <div className={`status ${winner ? "winner" : ""}`}>
+        {getStatusMessage()}
+        <button onClick={resetGame}>Reset Game</button>
+      </div>
 
-export default App
+      <div className="board">
+        {board.map((b, index) => {
+          const isWinningCell = winner && board[index] === winner; 
+          return (
+            <button
+              className={`cell ${isWinningCell ? "winning" : ""}`}
+              key={index}
+              onClick={() => handleClick(index)}
+              disabled={b !== null}
+            >
+              {b}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default App;
